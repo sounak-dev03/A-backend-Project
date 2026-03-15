3db7d0e46d75eeb14ff7862b69dab3026c50f708
Sounak Hazra <hazrasounak2003@gmail.com>
2026-03-15 18:03:10 +0530

    Connect to mongoDB atlas

---
+++ src/index.js
@@ -0,0 +1,37 @@
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+import connectDB from "./db/index_db.js";
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+
+
+
+/*
+import express from "express"
+const app = express()
+
+( async () => {
+    try 
+    {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log("Connected to MongoDB"); 
+        app.on("error", (error) => {
+            console.log("error: ", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+    } 
+    catch (error) 
+    {
+        console.error("Error connecting to MongoDB:", error);
+        throw error;
+    }
+})()
+*/
